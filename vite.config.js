66d92d7e3eda0bc7e38cd07172d52cb0973a39d7
Ahmad <swilaaam@gmail.com>
2025-08-17 15:40:35 +0200

    - Filament v4.0 - Refactor styles - Add new styles.css for theme variables and update app.css imports

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,3 @@
-// import { defineConfig } from 'vite';
-// import laravel from 'laravel-vite-plugin';
-//
-// export default defineConfig({
-//     plugins: [
-//         laravel({
-//             input: [
-//                 'resources/sass/app.scss',
-//                 'resources/js/app.js',
-//                 'resources/js/blog.js',
-//                 // 'resources/css/filament/dashboard/theme.css',
-//                 // 'resources/css/filament/admin/theme.css',
-//             ],
-//             refresh: true,
-//         }),
-//     ],
-// });
-
-
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
@@ -27,8 +8,6 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
-                // 'resources/css/filament/dashboard/theme.css',
-                // 'resources/css/filament/admin/theme.css',
             ],
             refresh: true,
         }),
