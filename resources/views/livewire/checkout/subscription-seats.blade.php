6c7d2510c295ed4d6b169058520bb76654ab863b
Ahmad <swilaaam@gmail.com>
2025-08-27 21:55:21 +0200

    - Update CSS configurations - Update libraries. - Improve styling for seat number input.

--- resources/views/livewire/checkout/subscription-seats.blade.php
+++ resources/views/livewire/checkout/subscription-seats.blade.php
@@ -1,13 +1,13 @@
 <div>
-    <div class="my-3 relative">
+    <div class="my-1 relative">
         <label class="form-control w-full max-w-xs">
-            <div class="label">
-                <span class="label-text">{{ __('Number of seats:') }}</span>
-            </div>
-            <input type="number" min="1"
-                  {{ $maxQuantity > 0 ? 'max=' . $maxQuantity : '' }}
-                   wire:model.blur="quantity"
-                   class="input input-bordered md:w-2/3 max-w-xs">
+            <fieldset class="fieldset">
+                <legend class="fieldset-legend">{{ __('Number of seats:') }}</legend>
+                <input type="number" min="1"
+                       {{ $maxQuantity > 0 ? 'max=' . $maxQuantity : '' }}
+                       wire:model.blur="quantity"
+                       class="input input-bordered md:w-2/3 max-w-xs">
+            </fieldset>
 
             <div class="absolute top-0 right-0 p-2">
                 <span wire:loading>
